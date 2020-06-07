779fb41f818a2658fd942bf9b7b7b196b587abac
Borna Sadeghi <sadeghiborna@gmail.com>
2020-06-07 10:12:18 -0400

    pls work

--- backend.py
+++ backend.py
@@ -1,15 +1,16 @@
 from flask import Flask, request
-# from classifier import interpret, classify
+from classifier import interpret, classify
 import json
 
 app = Flask (__name__)
 
 @app.route("/", methods=["GET"])
 def predict():
-    url = request.args["img_url"]
-
-    return "onoo"
-    # return json.dumps(interpret(classify(url)))
+    if request.args:
+        url = request.args["img_url"]
+        return json.dumps(interpret(classify(url)))
+    else:
+        return "Use the ?img_url= argument to choose an image by url"
 
 # then send a GET request to localhost:5000/?img_url=<IMAGE_URL>
 # curl https://applorange.herokuapp.com/?img_url=<IMAGE_URL>
